603721f79ddd281b7b3ea2bb78b7eeb7de543852
Mikhail Dedov <mdedov@gmail.com>
2024-12-02 14:11:02 +0400

    fix .gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,17 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
-    id("java")
+    application
+    jacoco
+    id("checkstyle")
 }
 
 group = "hexlet.code"
+
 version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.App") }
 
 repositories {
     mavenCentral()
@@ -16,4 +24,13 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
